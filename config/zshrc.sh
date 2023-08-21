9c8e4f73e379394d21dc56e286befd6eb3a9428d
erees1 <edward.r.rees@zoho.com>
2023-08-20 19:17:21 -0700

    added rlc and yk bins

--- config/zshrc.sh
+++ config/zshrc.sh
@@ -7,6 +7,15 @@ ZSH=$HOME/.oh-my-zsh
 
 plugins=(zsh-autosuggestions zsh-syntax-highlighting zsh-completions zsh-history-substring-search)
 
+function add_to_path() {
+    p=$1
+    if [[ "$PATH" != *"$p"* ]]; then
+      export PATH="$p:$PATH"
+    fi
+}
+
+add_to_path "${DOT_DIR}/bins"
+
 source $ZSH/oh-my-zsh.sh
 source $CONFIG_DIR/aliases.sh
 source $CONFIG_DIR/p10k.zsh
